694e8d20c2fc84df07a4d2bee9c94f2b4147056e
Oslowxscrx <oer.nogales@yavirac.edu.ec>
2023-05-30 18:55:55 -0500

    Templates RUI

---
+++ Proyecto-RUI/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { PagesComponent } from './pages/pages.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InsumosComponent } from './pages/insumos/insumos.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
+import { RolesComponent } from './pages/roles/roles.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SharedComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PagesComponent,
+    DashboardComponent,
+    InsumosComponent,
+    EmpleadosComponent,
+    RolesComponent,
+    UsuariosComponent,
+    NotFoundComponent,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
